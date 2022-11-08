5b82985682a3b7c4c13570bb3377c23370b1e4fd
Jaime Paredes <jparedes@imaginex.cl>
2022-11-07 23:25:26 -0300

    Endpoint estadoMundial

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -32,4 +32,18 @@ public class RestData {
 		response.setMensaje("Mensaje Recibido: " + message);
 		return response;
 	}
+
+	@GetMapping(path = "/estadoMundial", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Mundial getTotalMundial(){
+		LOGGER.log(Level.INFO, "Consulta mundial");
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> call = restTemplate.getForEntity("https://api.covid19api.com/world/total", String.class);
+		Mundial respose = new Mundial();
+		Gson gson = new Gson();
+		Mundial estado = gson.fromJson(call.getBody().toLowerCase(), Mundial.class);
+		response.setTotalConfirmed(estado.getTotalConfirmed());
+		response.setTotalDeaths(estado.getTotalDeaths());
+		response.setTotalRecovered(estado.getTotalRecovered());
+		return response;
+	}
 }
